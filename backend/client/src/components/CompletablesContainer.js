e95a119f6ea4459447126b7615d45927a4424266
oliverpdahl <oliverpdahl@gmail.com>
2020-06-23 11:10:31 -0400

    added compleatables container

---
+++ backend/client/src/components/CompletablesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CompletablesContainer extends Component {
+  render() {
+    return (
+      <div className="completables-container">
+        <h1>Test</h1>
+      </div>
+    )
+  }
+}
+
+export default CompletablesContainer
