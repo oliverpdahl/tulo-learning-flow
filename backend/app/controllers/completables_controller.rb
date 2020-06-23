f1dbcc29efed87f1ea93ae4463cd9f1e576c260d
oliverpdahl <oliverpdahl@gmail.com>
2020-06-23 10:14:08 -0400

    updated index and update

--- backend/app/controllers/completables_controller.rb
+++ backend/app/controllers/completables_controller.rb
@@ -1,7 +1,12 @@
 class CompletablesController < ApplicationController
   def index
+    completables = Completable.order("created_at DESC")
+    render json: completables
   end
 
   def update
+    completable = Completable.find(params[:id])
+    completable.update_attributes(completable_param)
+    render json: completable
   end
 end
