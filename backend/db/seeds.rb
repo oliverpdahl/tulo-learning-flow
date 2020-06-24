b3f9ba3f73a171d249e927b0bb045bc625edc0f8
oliverpdahl <oliverpdahl@gmail.com>
2020-06-24 17:41:31 -0400

    Added more titles

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Completable.create(title: "Learn to Ollie")
+Completable.create(title: 'Learn to Ollie')
+Completable.create(title: 'Learn Excell')
+Completable.create(title: 'Learn Compound Fractions')
