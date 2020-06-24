f3f5c4b9b9c8a3808ab722b933ac24d8a283e91b
oliverpdahl <oliverpdahl@gmail.com>
2020-06-24 12:14:53 -0400

    Add fetch completables

---
+++ backend/client/src/actions/fetchCompletables.js
@@ -0,0 +1,9 @@
+export const fetchCompletables = () => {
+  return (dispatch) => {
+    fetch('/api/v1/completables').then(response => {
+      return response.json()
+    }).then(responseJSON => {
+      dispatch({ type: 'LOAD_COMPLETABLES', completables: responseJSON })
+    })
+  }
+}
--- backend/client/src/components/CompletablesContainer.js
+++ backend/client/src/components/CompletablesContainer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCompletables } from ',/actions/fetchCompletables'
 
 class CompletablesContainer extends Component {
+  componentDidMount(){
+    console.log(this.props)
+    this.props.fetchCompletables()
+  }
+  
   render() {
     return (
       <div className="completables-container">
         <h1>Test</h1>
+        <ul className="completables">
+            {this.props.completables.map(completable =>{
+            return(
+              <li>{completable.title}</li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
 }
 
-export default CompletablesContainer
+const mapStateToProps = (state) => {
+  return { 
+    completables: state.completables
+  }
+}
+
+export default connect(mapStateToProps)(CompletablesContainer)
