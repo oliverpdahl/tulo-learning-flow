fdc6b7e3e130564fdf71c898bd18b0934fc26b19
oliverpdahl <oliverpdahl@gmail.com>
2020-06-25 14:21:20 -0400

    Changed layout bootstrap of title

--- backend/client/src/components/completableTitle.js
+++ backend/client/src/components/completableTitle.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card'
+import { Container, Row, Col } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
 
 class CompletableTitle extends Component {
   render(){
     return(
-      <Accordion.Toggle as={Card.Header} eventKey={this.props.completable.id} className="text-left">
-        {this.props.completable.title}
+      <Accordion.Toggle as={Card.Header} eventKey={this.props.completable.id}>
+        <Container fluid>
+          <Row>
+            <Col xs={0.5}>
+              <FontAwesomeIcon icon={faCircle} />
+            </Col>
+            <Col xs={7}>
+              {this.props.completable.title}
+            </Col>
+          </Row>
+        </Container>
       </Accordion.Toggle>
     )
   }
