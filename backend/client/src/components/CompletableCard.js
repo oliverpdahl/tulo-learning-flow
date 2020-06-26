2a076dd8873ee9bad31716b282cfa0c653637e41
oliverpdahl <oliverpdahl@gmail.com>
2020-06-26 10:03:46 -0400

    improved slugs

--- backend/client/src/components/CompletableCard.js
+++ backend/client/src/components/CompletableCard.js
@@ -3,15 +3,14 @@ import Card from 'react-bootstrap/Card'
 import CompletableTitle from './CompletableTitle'
 import CompletableContent from './CompletableContent'
 import { Link } from 'react-router-dom'
-
-var slugify = require('slugify')
+import slugify from 'slugify'
 
 class CompletableCard extends Component {
   render(){
     return(
       
         <Card>
-        <Link to={this.props.completable.title} style={{textDecoration: 'none'}}>
+        <Link to={slugify(this.props.completable.title).toLocaleLowerCase()} style={{textDecoration: 'none'}}>
           <CompletableTitle completable={this.props.completable}/>
           <CompletableContent completable={this.props.completable} toggleCompletable={this.props.toggleCompletable}/>
         </Link>
