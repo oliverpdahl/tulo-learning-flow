27a22bcd41e8ba0df7a9f4cd42b1af15db18bdc1
oliverpdahl <oliverpdahl@gmail.com>
2020-06-26 08:05:22 -0400

    added patch to toggleCompletale

--- backend/client/src/actions/toggleCompletable.js
+++ backend/client/src/actions/toggleCompletable.js
@@ -1,5 +1,13 @@
 export function toggleCompletable(completable) {
   return (dispatch) => {
     dispatch({ type: 'TOGGLE_COMPLETABLE', id: completable.id });
+    fetch('/api/v1/completables' + completable.id, {
+        method: 'PATCH',
+        mode: 'CORS',
+        body: JSON.stringify(completable),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).catch(err => err);
   };
 }
