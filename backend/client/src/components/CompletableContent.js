947d9894d6042a3963f9bce135716507ace31f0c
oliverpdahl <oliverpdahl@gmail.com>
2020-06-26 05:51:23 -0400

    Add complete button

--- backend/client/src/components/CompletableContent.js
+++ backend/client/src/components/CompletableContent.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card'
+import { Button, Col, Row, Container } from 'react-bootstrap'
 
 class CompletableContent extends Component {
   render(){
     return(
       <Accordion.Collapse eventKey={this.props.completable.id}>
-        <Card.Body></Card.Body>
+        <Card.Body>
+          <Container fluid>
+            <Row className="bottom-row">
+              <Col xs={12} sm={8} md={9}></Col>
+              <Col>
+                <Button variant="primary" block>Complete</Button>
+              </Col>
+            </Row>
+          </Container>
+        </Card.Body>
       </Accordion.Collapse>
     )
   }
