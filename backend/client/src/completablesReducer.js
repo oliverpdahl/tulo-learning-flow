bb211598f99ea9f54318b630adef2c6cc6746ac0
oliverpdahl <oliverpdahl@gmail.com>
2020-06-26 06:39:26 -0400

    Updated reducer to use create reducer

--- backend/client/src/completablesReducer.js
+++ backend/client/src/completablesReducer.js
@@ -1,29 +1,17 @@
-const completablesReducer = (state = { completables: [], requesting: false }, action) => {
-  switch (action.type) {
- 
-    case 'START_ADDING_COMPLETABLES_REQUEST':
-      return {
-        ...state,
-        completables: [...state.completables],
-        requesting: true
-      }
- 
-    case 'ADD_COMPLETABLES':
-      return {
-        ...state,
-        completables: action.completables,
-        requesting: false
-      }
-      
-    case 'TOGGLE_COMPLETED':
-      return {
-        ...state,
-        completables: action.completables,
-        requesting: false
-      }
-    default:
-      return state;
+import { createReducer } from '@reduxjs/toolkit'
+
+const initialState = { completables: [], requesting: false }
+const completablesReducer = createReducer(initialState, { 
+  START_ADDING_COMPLETABLES_REQUEST: (state) => {
+    state.requesting = true
+  },
+  ADD_COMPLETABLES: (state, action) => {
+    state.completables = action.completables
+  },
+  TOGGLE_COMPLETABLE: (state, action) => {
+    const completable = state.completables.find(completable => completable.id === action.id)
+    completable.complete = !completable.complete
   }
-};
+})
 
 export default completablesReducer;
