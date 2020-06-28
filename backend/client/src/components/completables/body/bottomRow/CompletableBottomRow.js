4f81c93a495e467ca4fad88fd9fd767c15d1187b
oliverpdahl <oliverpdahl@gmail.com>
2020-06-28 12:30:23 -0400

    format bottom row code

--- backend/client/src/components/completables/body/bottomRow/CompletableBottomRow.js
+++ backend/client/src/components/completables/body/bottomRow/CompletableBottomRow.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
-import { Button, Col, Row, Container } from 'react-bootstrap'
-import CompletableCompleteButton from './CompletableCompleteButton'
+import React, { Component } from "react";
+import { Button, Col, Row, Container } from "react-bootstrap";
+import CompletableCompleteButton from "./CompletableCompleteButton";
 
-class CompletableBottomRow extends Component {  
-  render(){
-    return(
+class CompletableBottomRow extends Component {
+  render() {
+    return (
       <Row className="bottom-row">
         <Col xs={12} sm={8} md={9}></Col>
         <Col>
-          <CompletableCompleteButton completable={this.props.completable} allCompletables={this.props.allCompletables} toggleCompletable={this.props.toggleCompletable}/>
+          <CompletableCompleteButton
+            completable={this.props.completable}
+            allCompletables={this.props.allCompletables}
+            toggleCompletable={this.props.toggleCompletable}
+          />
         </Col>
       </Row>
-    )
+    );
   }
 }
 
-export default CompletableBottomRow
+export default CompletableBottomRow;
--- backend/client/src/components/completables/body/bottomRow/CompletableCompleteButton.js
+++ backend/client/src/components/completables/body/bottomRow/CompletableCompleteButton.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react'
-import { Button, Col, Row, Container } from 'react-bootstrap'
-import CompletablesContainer from '../../CompletablesContainer'
+import React, { Component } from "react";
+import { Button, Col, Row, Container } from "react-bootstrap";
+import CompletablesContainer from "../../CompletablesContainer";
 
 class CompletableCompleteButton extends Component {
   handleOnClick = () => {
-    const allC = this.props.allCompletables.find(c => c.id === this.props.completable.id)
-    this.props.toggleCompletable(allC)
-  }
-  
-  render(){
-    return(
-      <Button variant="primary" onClick={this.handleOnClick} block>Complete</Button>
-    )
+    const allC = this.props.allCompletables.find(
+      (c) => c.id === this.props.completable.id
+    );
+    this.props.toggleCompletable(allC);
+  };
+
+  render() {
+    return (
+      <Button variant="primary" onClick={this.handleOnClick} block>
+        Complete
+      </Button>
+    );
   }
 }
 
-export default CompletableCompleteButton
+export default CompletableCompleteButton;
