b675657d2082cdef9eb1a106d1682af377b5dcde
oliverpdahl <oliverpdahl@gmail.com>
2020-06-28 08:07:54 -0400

    Content blocks of nodes appear

--- backend/app/controllers/completables_controller.rb
+++ backend/app/controllers/completables_controller.rb
@@ -1,7 +1,9 @@
 class CompletablesController < ApplicationController
   def index
     paths = Path.all
-    render json: paths.to_json(include: [:content_blocks, :nodes])
+    render json: paths.to_json(:include => [:content_blocks, :nodes => { 
+      :include => :content_blocks
+     }])
   end
 
   def toggle_complete
