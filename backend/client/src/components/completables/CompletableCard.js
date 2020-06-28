c78664f32a5db786357cdab2ccb17852f5d9fff9
oliverpdahl <oliverpdahl@gmail.com>
2020-06-28 12:40:43 -0400

    format components

--- backend/client/src/components/completables/CompletableCard.js
+++ backend/client/src/components/completables/CompletableCard.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
-import Card from 'react-bootstrap/Card'
-import CompletableHeader from './header/CompletableHeader'
-import CompletableBody from './body/CompletableBody'
-import { Link } from 'react-router-dom'
-import slugify from 'slugify'
+import React, { Component } from "react";
+import Card from "react-bootstrap/Card";
+import CompletableHeader from "./header/CompletableHeader";
+import CompletableBody from "./body/CompletableBody";
+import { Link } from "react-router-dom";
+import slugify from "slugify";
 
 class CompletableCard extends Component {
-  render(){
-    return(
-        <Card>
-          <Link to={slugify(this.props.completable.title).toLocaleLowerCase()} style={{textDecoration: 'none'}}>
-            <CompletableHeader completable={this.props.completable} allCompletables={this.props.allCompletables} />
-            <CompletableBody completable={this.props.completable} allCompletables={this.props.allCompletables} toggleCompletable={this.props.toggleCompletable}/>
-          </Link>
-        </Card>
-    )
+  render() {
+    return (
+      <Card>
+        <Link
+          to={slugify(this.props.completable.title).toLocaleLowerCase()}
+          style={{ textDecoration: "none" }}
+        >
+          <CompletableHeader
+            completable={this.props.completable}
+            allCompletables={this.props.allCompletables}
+          />
+          <CompletableBody
+            completable={this.props.completable}
+            allCompletables={this.props.allCompletables}
+            toggleCompletable={this.props.toggleCompletable}
+          />
+        </Link>
+      </Card>
+    );
   }
 }
 
-export default CompletableCard
+export default CompletableCard;
--- backend/client/src/components/completables/CompletablesContainer.js
+++ backend/client/src/components/completables/CompletablesContainer.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react'
-import Accordion from 'react-bootstrap/Accordion';
-import CompletableCard from './CompletableCard';
-import { connect } from 'react-redux'
-import { toggleCompletable } from '../../actions/toggleCompletable'
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import CompletableCard from "./CompletableCard";
+import { connect } from "react-redux";
+import { toggleCompletable } from "../../actions/toggleCompletable";
 
 class CompletablesContainer extends Component {
   listCompletables = () => {
-    return(
-      !!this.props.localCompletables.map && this.props.localCompletables.map(completable => <CompletableCard key={completable.id} completable={completable} allCompletables={this.props.allCompletables} toggleCompletable={this.props.toggleCompletable}/>)
-    )
-  }
-  render(){
-    return(
-    <Accordion defaultActiveKey="0">
-      {this.listCompletables()}
-    </Accordion>
-    )
+    return (
+      !!this.props.localCompletables.map &&
+      this.props.localCompletables.map((completable) => (
+        <CompletableCard
+          key={completable.id}
+          completable={completable}
+          allCompletables={this.props.allCompletables}
+          toggleCompletable={this.props.toggleCompletable}
+        />
+      ))
+    );
+  };
+  render() {
+    return (
+      <Accordion defaultActiveKey="0">{this.listCompletables()}</Accordion>
+    );
   }
 }
 
-export default connect(null, {toggleCompletable})(CompletablesContainer)
+export default connect(null, { toggleCompletable })(CompletablesContainer);
