089131d39d1bb618e2093a62cc2635a4da89c336
oliverpdahl <oliverpdahl@gmail.com>
2020-06-28 12:29:41 -0400

    format actions

--- backend/client/src/actions/fetchCompletables.js
+++ backend/client/src/actions/fetchCompletables.js
@@ -1,10 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 export function fetchCompletables() {
   return (dispatch) => {
-    dispatch({ type: 'START_ADDING_COMPLETABLES_REQUEST' });
-    return axios.get('/api/v1/completables.json')
-      .then(response => response.data)
-      .then(completables => dispatch({ type: 'ADD_COMPLETABLES', completables }));
+    dispatch({ type: "START_ADDING_COMPLETABLES_REQUEST" });
+    return axios
+      .get("/api/v1/completables.json")
+      .then((response) => response.data)
+      .then((completables) =>
+        dispatch({ type: "ADD_COMPLETABLES", completables })
+      );
   };
 }
--- backend/client/src/actions/toggleCompletable.js
+++ backend/client/src/actions/toggleCompletable.js
@@ -1,9 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 export function toggleCompletable(completable) {
   return (dispatch) => {
-    return axios.patch(`/api/v1/completables/complete/${completable.id}.json`, completable)
-    .then(res => res.data)
-    .then(data => dispatch({ type: 'TOGGLE_COMPLETABLE', id: data.id }))
+    return axios
+      .patch(
+        `/api/v1/completables/complete/${completable.id}.json`,
+        completable
+      )
+      .then((res) => res.data)
+      .then((data) => dispatch({ type: "TOGGLE_COMPLETABLE", id: data.id }));
   };
 }
