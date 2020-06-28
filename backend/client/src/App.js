4d3c30291d90596011d20d3d68999023da1b2c12
oliverpdahl <oliverpdahl@gmail.com>
2020-06-28 12:45:11 -0400

    Client cleanup

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -1,45 +1,49 @@
-import { fetchCompletables } from './actions/fetchCompletables'
-import { fetchPaths } from './actions/fetchPaths'
-import CompletablesContainer from './components/completables/CompletablesContainer'
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom';
+import { fetchCompletables } from "./actions/fetchCompletables";
+import { fetchPaths } from "./actions/fetchPaths";
+import CompletablesContainer from "./components/completables/CompletablesContainer";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class App extends Component {
-
   componentDidMount() {
-    this.props.fetchCompletables()
-    this.props.fetchPaths()
+    this.props.fetchCompletables();
+    this.props.fetchPaths();
   }
 
   handleLoading = () => {
-    if(this.props.loading) {
-      return <div>Loading...</div>
+    if (this.props.loading) {
+      return <div>Loading...</div>;
     } else {
-      console.log(this.props.completables)
-      return <CompletablesContainer localCompletables={this.props.paths} allCompletables={this.props.completables}/>
+      console.log(this.props.completables);
+      return (
+        <CompletablesContainer
+          localCompletables={this.props.paths}
+          allCompletables={this.props.completables}
+        />
+      );
     }
-  }
+  };
 
   render() {
     return (
       <Router>
-        <Link to='/paths' style={{textDecoration: 'none'}}>{this.handleLoading()}</Link>
+        <Link to="/paths" style={{ textDecoration: "none" }}>
+          {this.handleLoading()}
+        </Link>
       </Router>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = state => {
-  return { 
+const mapDispatchToProps = (state) => {
+  return {
     completables: state.completables,
     paths: state.paths,
-    loading: state.loading
-  }
-}
+    loading: state.loading,
+  };
+};
 
-export default connect(mapDispatchToProps, {fetchCompletables, fetchPaths})(App)
+export default connect(mapDispatchToProps, { fetchCompletables, fetchPaths })(
+  App
+);
