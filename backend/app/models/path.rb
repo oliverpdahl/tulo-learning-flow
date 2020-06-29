6fe6e3cdbd0f869697fb361b4b2e8f91c6f6d4bc
oliverpdahl <oliverpdahl@gmail.com>
2020-06-29 06:50:59 -0400

    add num nodes complete to path

--- backend/app/models/path.rb
+++ backend/app/models/path.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal: true
+
 class Path < Completable
   has_many :nodes
+  attr_accessor :num_nodes_complete
+  after_save :update_num_nodes_complete
+
+  def update_num_nodes_complete
+    complete = this.nodes.count { |n| n.complete == true }
+    count = this.nodes.length
+    [count, complete]
+  end
 end
