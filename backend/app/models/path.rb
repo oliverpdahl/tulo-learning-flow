22efbd5f5503b818683fc9dd1143984b02415c16
oliverpdahl <oliverpdahl@gmail.com>
2020-06-29 07:27:19 -0400

    discard num nodes complete

--- backend/app/models/path.rb
+++ backend/app/models/path.rb
@@ -2,12 +2,4 @@
 
 class Path < Completable
   has_many :nodes
-  attr_accessor :num_nodes_complete
-  after_save :update_num_nodes_complete
-
-  def update_num_nodes_complete
-    complete = this.nodes.count { |n| n.complete == true }
-    count = this.nodes.length
-    [count, complete]
-  end
 end
