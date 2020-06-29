5e9d06ab476255305788c43c0a2d7df029a08c32
oliverpdahl <oliverpdahl@gmail.com>
2020-06-29 14:16:57 -0400

    Formating good

--- backend/client/src/components/navBar/NavBarContainer.js
+++ backend/client/src/components/navBar/NavBarContainer.js
@@ -4,25 +4,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faQuestionCircle as help,
   faUserAstronaut as user,
+  faMeteor as path,
 } from "@fortawesome/free-solid-svg-icons";
 
 class NavBarContainer extends Component {
   render() {
     return (
-      <div className="sideBar bg-primary fill">
+      <div className="sideBar bg-primary fill pt-1">
         <br />
         <h2 className="text-light text-center font-weight-bold">
           T<br />U<br />L<br />O
         </h2>
-        <Nav defaultActiveKey="/home" className="flex-column text-light">
+        <Nav defaultActiveKey="/home" className="flex-column text-light pt-4">
           <Nav.Link className="text-light" href="/home">
-            Active
+            <FontAwesomeIcon icon={path} size="2x" />
           </Nav.Link>
           <Nav.Link className="text-light text-center" eventKey="link-1">
-            <FontAwesomeIcon icon={user} size="xl" />
+            <FontAwesomeIcon icon={user} size="2x" />
           </Nav.Link>
           <Nav.Link eventKey="link-2" className="text-light text-center">
-            <FontAwesomeIcon icon={help} size="xl" />
+            <FontAwesomeIcon icon={help} size="2x" />
           </Nav.Link>
         </Nav>
       </div>
