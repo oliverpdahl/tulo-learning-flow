0cf2128fcfd6bf4f06acc050a6264d1d5bf5af61
oliverpdahl <oliverpdahl@gmail.com>
2020-06-29 07:40:05 -0400

    Add Completable progrress bar

---
+++ backend/client/src/components/completables/header/CompletableProgressBar.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import { ProgressBar } from "react-bootstrap";
+
+class CompletableProgressBar extends Component {
+  render() {
+    return <ProgressBar now={69} />;
+  }
+}
+
+export default CompletableProgressBar;
