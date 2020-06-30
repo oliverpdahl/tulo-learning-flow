a6df3f315d08ef2816e5ff1b1ba343f5b7eedfe5
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 09:27:09 -0400

    about to help

---
+++ backend/client/src/components/about/AboutContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AboutContainer extends Component {
+  render() {
+    return <h1>About</h1>;
+  }
+}
+
+export default AboutContainer;
--- backend/client/src/components/help/helpContainer.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class HelpContainer extends Component {
-  render() {
-    return <h1>Help</h1>;
-  }
-}
-
-export default HelpContainer;
