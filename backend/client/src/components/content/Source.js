9b8b0494b99dfd2188164bd3a6e7fcfebbfd8ac1
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 18:30:44 -0400

    contitional source

--- backend/client/src/components/content/Source.js
+++ backend/client/src/components/content/Source.js
@@ -4,10 +4,12 @@ import { Col } from "react-bootstrap";
 class Source extends Component {
   render() {
     return (
-      <footer class="blockquote-footer text-right pb-3">
-        {this.props.creator}{" "}
-        <cite title="Source Title">{this.props.resource}</cite>
-      </footer>
+      !!(this.props.resource || this.props.creator) && (
+        <footer class="blockquote-footer text-right pb-3">
+          {this.props.creator}{" "}
+          <cite title="Source Title">{this.props.resource}</cite>
+        </footer>
+      )
     );
   }
 }
