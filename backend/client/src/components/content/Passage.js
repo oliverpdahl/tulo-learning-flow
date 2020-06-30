3fa8e3337ceceb2a34de4e3945ff6ad17c19ead8
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 11:42:51 -0400

    conditional columns

--- backend/client/src/components/content/Passage.js
+++ backend/client/src/components/content/Passage.js
@@ -6,17 +6,24 @@ class Passage extends Component {
   render() {
     return (
       <Row>
-        <Col xs={12} lg={4} className="image">
-          <Image src={this.props.img_link} fluid />
-          <Source
-            creator={this.props.img_creator}
-            resource={this.props.img_resource}
-          />
-        </Col>
-        <Col className="text">
-          <p>{this.props.text}</p>
-          <Source creator={this.props.creator} resource={this.props.resource} />
-        </Col>
+        {!!this.props.img_link && (
+          <Col xs={12} lg={4} className="image">
+            <Image src={this.props.img_link} fluid />
+            <Source
+              creator={this.props.img_creator}
+              resource={this.props.img_resource}
+            />
+          </Col>
+        )}
+        {!!this.props.text && (
+          <Col className="text">
+            <p>{this.props.text}</p>
+            <Source
+              creator={this.props.creator}
+              resource={this.props.resource}
+            />
+          </Col>
+        )}
       </Row>
     );
   }
