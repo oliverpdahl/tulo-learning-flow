acb8b1c242c3a0c826b0cc99df09e54e13ae41b3
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 12:57:17 -0400

    Added video

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,22 +9,35 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 kf = Path.create(title: 'Learn to Kickflip')
 kf_content = kf.content_blocks.create
-kf_content.contents.create(text: %{The ollie (invented by Alan "Ollie" Gelfand in 1977) is the first trick that most skateboarders learn. It's a leap into the air on the skateboard with the skater's feet still on the board, even in mid-air. The ollie is a great technique for jumping over obstacles and impressing friends even as you're still learning the basics. Learning to ollie makes sense, as the ollie is the foundation of almost all flatland and park skateboarding tricks. Once you learn how to ollie, you’ll be able to move on to learning all kinds of other skateboarding tricks or inventing your own.},
-                           img_link: 'https://www.liveabout.com/thmb/WgWcAwnniC0GklU8FBzBGCcvJ3w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-641276018-591dc0935f9b58f4c04a8452.jpg',
-                           img_creator: 'Tony Anderson',
-                           img_resource: 'GettyImages',
-                           link: 'https://www.liveabout.com/how-to-ollie-on-skateboard-3002977',
-                           creator: 'Steve Cave',
-                           resource: 'liveabout',
-                           type: 'Passage')
+kf_content.contents.create(
+  text: %{The ollie (invented by Alan "Ollie" Gelfand in 1977) is the first trick that most skateboarders learn. It's a leap into the air on the skateboard with the skater's feet still on the board, even in mid-air. The ollie is a great technique for jumping over obstacles and impressing friends even as you're still learning the basics. Learning to ollie makes sense, as the ollie is the foundation of almost all flatland and park skateboarding tricks. Once you learn how to ollie, you’ll be able to move on to learning all kinds of other skateboarding tricks or inventing your own.},
+  img_link: 'https://www.liveabout.com/thmb/WgWcAwnniC0GklU8FBzBGCcvJ3w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-641276018-591dc0935f9b58f4c04a8452.jpg',
+  img_creator: 'Tony Anderson',
+  img_resource: 'GettyImages',
+  link: 'https://www.liveabout.com/how-to-ollie-on-skateboard-3002977',
+  creator: 'Steve Cave',
+  resource: 'liveabout',
+  type: 'Passage'
+)
 fssi = kf.nodes.create(title: 'Learn to Front Side Shove It')
 hf = kf.nodes.create(title: 'Learn to Heelflip')
 ollie = kf.nodes.create(title: 'Learn to Ollie', complete: true)
 ollie_content = ollie.content_blocks.create
-ollie_content.contents.create(text: %{The ollie (invented by Alan "Ollie" Gelfand in 1977) is the first trick that most skateboarders learn. It's a leap into the air on the skateboard with the skater's feet still on the board, even in mid-air. The ollie is a great technique for jumping over obstacles and impressing friends even as you're still learning the basics. Learning to ollie makes sense, as the ollie is the foundation of almost all flatland and park skateboarding tricks. Once you learn how to ollie, you’ll be able to move on to learning all kinds of other skateboarding tricks or inventing your own.},
-                              link: 'https://www.liveabout.com/thmb/WgWcAwnniC0GklU8FBzBGCcvJ3w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-641276018-591dc0935f9b58f4c04a8452.jpg',
-                              creator: 'Steve Cave',
-                              resource: 'liveabout',
-                              type: 'Passage')
+ollie_content.contents.create(
+  text: %{The ollie (invented by Alan "Ollie" Gelfand in 1977) is the first trick that most skateboarders learn. It's a leap into the air on the skateboard with the skater's feet still on the board, even in mid-air. The ollie is a great technique for jumping over obstacles and impressing friends even as you're still learning the basics. Learning to ollie makes sense, as the ollie is the foundation of almost all flatland and park skateboarding tricks. Once you learn how to ollie, you’ll be able to move on to learning all kinds of other skateboarding tricks or inventing your own.},
+  img_link: 'https://www.liveabout.com/thmb/WgWcAwnniC0GklU8FBzBGCcvJ3w=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-641276018-591dc0935f9b58f4c04a8452.jpg',
+  img_creator: 'Tony Anderson',
+  img_resource: 'GettyImages',
+  link: 'https://www.liveabout.com/how-to-ollie-on-skateboard-3002977',
+  creator: 'Steve Cave',
+  resource: 'liveabout',
+  type: 'Passage'
+)
+ollie_content.contents.create(
+  link: 'https://cdn.jwplayer.com/videos/L4prgIC5-qP58AAan.mp4',
+  creator: 'Nate Cohen',
+  resource: 'HowCast',
+  type: 'Video'
+)
 Path.create(title: 'Learn Excell')
 Path.create(complete: true, title: 'Learn Compound Fractions')
