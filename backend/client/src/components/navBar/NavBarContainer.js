a4922411401a36eab56d81a9348c8921715567b1
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 12:29:52 -0400

    sticky nav bar

--- backend/client/src/components/navBar/NavBarContainer.js
+++ backend/client/src/components/navBar/NavBarContainer.js
@@ -4,20 +4,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faQuestionCircle as about,
   faUserAstronaut as user,
-  faMeteor as paths,
+  faMeteor as path,
 } from "@fortawesome/free-solid-svg-icons";
 import NavBarLink from "./NavBarLink";
 
 class NavBarContainer extends Component {
   render() {
     return (
-      <div className="sideBar bg-primary fill pt-1">
+      <div className="sideBar bg-primary fill pt-1 sticky-top">
         <br />
         <h2 className="text-light text-center font-weight-bold">
           T<br />U<br />L<br />O
         </h2>
         <Nav defaultActiveKey="/home" className="flex-column text-light pt-4">
-          <NavBarLink icon={paths} link={"/paths"} />
+          <NavBarLink icon={path} link={"/paths"} />
           <NavBarLink icon={user} link={"/user"} />
           <NavBarLink icon={about} link={"/about"} />
         </Nav>
