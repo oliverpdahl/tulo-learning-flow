bdde6f7a281d2028856060f641a8662bb270c899
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 18:17:07 -0400

    user page col

--- backend/client/src/components/user/userContainer.js
+++ backend/client/src/components/user/userContainer.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 class UserContainer extends Component {
   render() {
-    return <h1>User</h1>;
+    return (
+      <Col>
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1>Login</h1>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
+    );
   }
 }
 
