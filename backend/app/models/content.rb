316900e77b2bf7d6eb21380dcc88f653e5030219
oliverpdahl <oliverpdahl@gmail.com>
2020-06-30 10:02:56 -0400

    content and content block relationship

--- backend/app/models/content.rb
+++ backend/app/models/content.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Content < ApplicationRecord
+  belongs_to :content_block
 end
--- backend/app/models/content_block.rb
+++ backend/app/models/content_block.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class ContentBlock < ApplicationRecord
   belongs_to :completable
+  has_many :contents
 end
