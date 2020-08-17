3ba39826bd94a99115fc4d784470dac190cc94f3
oliverpdahl <oliverpdahl@gmail.com>
2020-08-17 12:33:12 -0400

    Turning completables into a functional component

--- backend/client/src/components/completables/CompletablesContainer.js
+++ backend/client/src/components/completables/CompletablesContainer.js
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
-import { Accordion, Col } from "react-bootstrap";
+import { Accordion, Col, Button } from "react-bootstrap";
 import CompletableCard from "./CompletableCard";
 import { connect } from "react-redux";
 import { toggleCompletable } from "../../actions/toggleCompletable";
 
-class CompletablesContainer extends Component {
-  listCompletables = () => {
+function CompletablesContainer(props) {
+  const listCompletables = () => {
     return (
-      !!this.props.localCompletables.map &&
-      this.props.localCompletables.map((completable) => (
+      !!props.localCompletables.map &&
+      props.localCompletables.map((completable) => (
         <CompletableCard
           eventKey={completable.id}
           completable={completable}
-          allCompletables={this.props.allCompletables}
-          toggleCompletable={this.props.toggleCompletable}
-          isComplete={this.isComplete}
+          allCompletables={props.allCompletables}
+          toggleCompletable={props.toggleCompletable}
+          isComplete={isComplete}
         />
       ))
     );
   };
 
-  isComplete = (completable) => {
-    return this.props.allCompletables.find((c) => c.id === completable.id)
-      .complete;
+  const isComplete = (completable) => {
+    return props.allCompletables.find((c) => c.id === completable.id).complete;
   };
 
-  render() {
-    return (
-      <Col className="p-0">
-        <Accordion>{this.listCompletables()}</Accordion>
-      </Col>
-    );
-  }
+  return (
+    <Col className="p-0">
+      <Accordion>{listCompletables()}</Accordion>
+    </Col>
+  );
 }
 
 export default connect(null, { toggleCompletable })(CompletablesContainer);
