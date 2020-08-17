baa792d6556198e69d443f0a174d5120271ba056
oliverpdahl <oliverpdahl@gmail.com>
2020-08-17 14:58:41 -0400

    user container to functional comp

--- backend/client/src/components/user/userContainer.js
+++ backend/client/src/components/user/userContainer.js
@@ -1,42 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
 import { Col, Container, Row, Form, Button } from "react-bootstrap";
 
-class UserContainer extends Component {
-  render() {
-    return (
-      <Col>
-        <Container fluid>
-          <Row>
-            <Col className="pt-3">
-              <h1>Login</h1>
-              <Form>
-                <Form.Group controlId="email">
-                  <Form.Control
-                    size="lg"
-                    type="email"
-                    placeholder="Enter email"
-                  />
-                  <Form.Text className="text-muted">
-                    Please enter your @evace.org email
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group controlId="password">
-                  <Form.Control
-                    size="lg"
-                    type="password"
-                    placeholder="Enter password"
-                  />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Log In
-                </Button>
-              </Form>
-            </Col>
-          </Row>
-        </Container>
-      </Col>
-    );
-  }
+export default function UserContainer() {
+  return (
+    <Col>
+      <Container fluid>
+        <Row>
+          <Col className="pt-3">
+            <h1>Login</h1>
+            <Form>
+              <Form.Group controlId="email">
+                <Form.Control
+                  size="lg"
+                  type="email"
+                  placeholder="Enter email"
+                />
+                <Form.Text className="text-muted">
+                  Please enter your @evace.org email
+                </Form.Text>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Control
+                  size="lg"
+                  type="password"
+                  placeholder="Enter password"
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Log In
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Col>
+  );
 }
-
-export default UserContainer;
