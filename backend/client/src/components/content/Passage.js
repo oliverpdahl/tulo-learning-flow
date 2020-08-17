137bc297351ca0e2472a5f2fb960aa2f52679493
oliverpdahl <oliverpdahl@gmail.com>
2020-08-17 14:39:00 -0400

    passage to functional comp

--- backend/client/src/components/content/Passage.js
+++ backend/client/src/components/content/Passage.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import { Row, Col, Image } from "react-bootstrap";
 import Source from "./Source";
 
-class Passage extends Component {
-  render() {
-    return (
-      <Row>
-        {!!this.props.img_link && (
-          <Col xs={12} sm={6} md={4} className="image">
-            <Image src={this.props.img_link} fluid />
-            <Source
-              creator={this.props.img_creator}
-              resource={this.props.img_resource}
-            />
-          </Col>
-        )}
-        {!!this.props.text && (
-          <Col className="text text-dark">
-            <p>
-              {this.props.text}
-              <Source
-                creator={this.props.creator}
-                resource={this.props.resource}
-              />
-            </p>
-          </Col>
-        )}
-      </Row>
-    );
-  }
+function Passage({
+  img_link,
+  img_creator,
+  img_resource,
+  text,
+  creator,
+  resource,
+}) {
+  return (
+    <Row>
+      {!!img_link && (
+        <Col xs={12} sm={6} md={4} className="image">
+          <Image src={img_link} fluid />
+          <Source creator={img_creator} resource={img_resource} />
+        </Col>
+      )}
+      {!!text && (
+        <Col className="text text-dark">
+          <p>
+            {text}
+            <Source creator={creator} resource={resource} />
+          </p>
+        </Col>
+      )}
+    </Row>
+  );
 }
 
 export default Passage;
