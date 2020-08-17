1058fd4292c383aa820f5d658eaf9f40b049b553
oliverpdahl <oliverpdahl@gmail.com>
2020-08-17 13:47:59 -0400

    completable complete status to functional componen

--- backend/client/src/components/completables/header/CompletableCompleteStatus.js
+++ backend/client/src/components/completables/header/CompletableCompleteStatus.js
@@ -1,21 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle as fasCircle } from "@fortawesome/free-solid-svg-icons";
 import { faCircle as farCircle } from "@fortawesome/free-regular-svg-icons";
 import { Col } from "react-bootstrap";
 
-class CompletableCompleteStatus extends Component {
-  render() {
-    return (
-      <Col xs={0.5}>
-        {this.props.isComplete(this.props.completable) ? (
-          <FontAwesomeIcon icon={fasCircle} />
-        ) : (
-          <FontAwesomeIcon icon={farCircle} />
-        )}
-      </Col>
-    );
-  }
+export default function CompletableCompleteStatus({ isComplete, completable }) {
+  const fullShape = <FontAwesomeIcon icon={fasCircle} />;
+  const emptyShape = <FontAwesomeIcon icon={farCircle} />;
+  return <Col xs={0.5}>{isComplete(completable) ? fullShape : emptyShape}</Col>;
 }
-
-export default CompletableCompleteStatus;
